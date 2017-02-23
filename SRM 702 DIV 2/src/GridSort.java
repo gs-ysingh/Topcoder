433d23c652f87ad6d8b809e47b95adafb80f4f96
Yogesh Singh <ysingh@gainsight.com>
2017-02-24 01:42:04 +0530

    Re-written the code, Matrix traversal need to think quick

--- SRM 702 DIV 2/src/GridSort.java
+++ SRM 702 DIV 2/src/GridSort.java
@@ -5,41 +5,64 @@ import static java.lang.Math.*;
 public class GridSort {
 	
 	public String sort(int n, int m, int[] grid) {
-		int flag = 0;
+		String res = "Possible";
+		int [][] values = new int[n][m];
 
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				values[i][j] = grid[i* m + j];
+			}
+		}
+
+		int flag = 1;
 		for (int i = 0; i < n - 1; i++) {
-			flag = 0;
 			for (int j = 0; j < m; j++) {
 				if(j == 0) {
-					if(grid[i*m + j] > grid[i*m + j + m]) {
+					if(values[i][j] > values[i + 1][j]) {
 						flag = 1;
+					}
+					else {
+						flag = 0;
 					}
 				}
 				else {
-					if(grid[i*m + j] < grid[i*m + j + m] && flag == 1
-							|| grid[i*m + j] > grid[i*m + j + m] && flag == 0) {
-						return "Impossible";
+					if((flag == 0 && values[i][j] > values[i + 1][j])
+							|| (flag == 1 && values[i][j] < values[i + 1][j])) {
+						res = "Impossible";
+						break;
 					}
 				}
+			}
+			if(res == "Impossible") {
+				break;
 			}
 		}
 
 		for (int i = 0; i < m - 1; i++) {
-			flag = 0;
 			for (int j = 0; j < n; j++) {
 				if(j == 0) {
-					if(grid[j*m + i] > grid[j*m + i + 1]) {
+					if(values[j][i] > values[j][i + 1]) {
 						flag = 1;
+					}
+					else {
+						flag = 0;
 					}
 				}
 				else {
-					if(grid[j*m + i] < grid[j*m + i + 1] && flag == 1
-							|| grid[j*m + i] > grid[j*m + i + 1] && flag == 0) {
-						return "Impossible";
+					if((values[j][i] > values[j][i + 1] && flag == 0)
+							|| (values[j][i] < values[j][i + 1] && flag == 1)) {
+						res = "Impossible";
+						break;
 					}
 				}
 			}
+
+			if(res == "Impossible") {
+				break;
+			}
+			
 		}
-		return "Possible";
+
+		return res;
 	}
 }
