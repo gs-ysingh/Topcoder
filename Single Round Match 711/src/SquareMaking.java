b0b19e6b3710e4b59dfbc03029a9ebd8143a8574
Yogesh Singh <ysingh@gainsight.com>
2017-03-26 20:35:50 +0530

    Refactoring of code

--- Single Round Match 711/src/SquareMaking.java
+++ Single Round Match 711/src/SquareMaking.java
@@ -5,20 +5,22 @@ import static java.lang.Math.*;
 public class SquareMaking {
 	
 	public int getMinimalPrice(int a, int b, int c, int d) {
-		int [] arr = new int[4];
-		int p = Math.abs(a - b) +  Math.abs(a - c) +  Math.abs(a - d);
-		int q = Math.abs(b - a) +  Math.abs(b - c) +  Math.abs(b - d);
-		int r = Math.abs(c - a) +  Math.abs(c - b) +  Math.abs(c - d);
-		int s = Math.abs(d - b) +  Math.abs(d - a) +  Math.abs(d - c);
-
-		arr[0] = p;
-		arr[1] = q;
-		arr[2] = r;
-		arr[3] = s;
+		int size = 4;
+		int [] arr = new int[size];
+		arr[0] = a;
+		arr[1] = b;
+		arr[2] = c;
+		arr[3] = d;
 
 		int min = Integer.MAX_VALUE;
-		for (int i = 0; i < 3; i++) {
-			min = Math.min(arr[i], min);
+		int sum = 0;
+
+		for (int i = 0; i < size; i++) {
+			sum = 0;
+			for (int j = 0; j < size; j++) {
+				sum = sum + Math.abs(arr[i] - arr[j]);
+			}
+			min = Math.min(sum, min);
 		}
 
 		return min;
